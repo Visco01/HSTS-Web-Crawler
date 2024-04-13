2d6cd0cebb849400a91eb1d18286f75902348cff
Visco01 <visconti373@gmail.com>
2024-04-13 16:27:30 +0200

    Add wrong_policy column

--- import_tranco_list.py
+++ import_tranco_list.py
@@ -14,30 +14,33 @@ c = conn.cursor()
 
 c.execute('''DROP TABLE IF EXISTS sites''')
 c.execute('''CREATE TABLE IF NOT EXISTS sites
-             (rank INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, HSTS BOOLEAN, max_age INTEGER, include_subdomains BOOLEAN, preload BOOLEAN)''')
+             (rank INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, HSTS BOOLEAN, max_age LONG, include_subdomains BOOLEAN, preload BOOLEAN, wrong_policy TEXT)''')
 
 with sync_playwright() as p:
     browser = p.chromium.launch()
     context = browser.new_context()
     page = context.new_page()
-    for domain in latest_list.top(10):
+    for domain in latest_list.top(100):
         url = "http://www." + domain if not domain.startswith("www.") else domain
         try:
             response = page.goto(url)
             hs = response.headers.get("strict-transport-security")
-            hs_array = hs.split("; ") if hs is not None else None
+            hs_array = hs.split(";") if hs is not None else None
             print(hs_array)
         except Exception as e:
             print(f"Error collecting HSTS policy for {domain}: {e}")
-        c.execute("INSERT INTO sites (url, HSTS, max_age, include_subdomains, preload) VALUES (?, ?, ?, ?, ?)",
-        (url,
-         hs is not None,
-         int(hs_array[0].split("=")[1]) if hs is not None else None,
-         "includeSubdomains" in hs_array if hs is not None else False,
-         "preload" in hs_array if hs is not None else False,))
+            continue
+        try:
+            c.execute("INSERT INTO sites (url, HSTS, max_age, include_subdomains, preload) VALUES (?, ?, ?, ?, ?)",
+            (url,
+             hs is not None,
+             int(hs_array[0].split("=")[1]) if hs is not None else None,
+             " includeSubdomains" in hs_array if hs is not None else False,
+             " preload" in hs_array if hs is not None else False,))
+        except Exception as e:
+            continue
         hs = None
 
-# Commit changes and close the connection
 conn.commit()
 conn.close()
 
