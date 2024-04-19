577b9a5bc892be70643dda0328fcb730fddb9b38
Visco01 <visconti373@gmail.com>
2024-04-19 08:37:30 +0200

    Fix bash command

--- run_crawler.sh
+++ run_crawler.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env sh
-
-# Run commands in parallel
-python3 hsts_web_crawler.py firefox &
-python3 hsts_web_crawler.py chromium &
-
-# Wait for all background processes to finish
-wait
+python3 hsts_web_crawler.py firefox
+python3 hsts_web_crawler.py chromium
